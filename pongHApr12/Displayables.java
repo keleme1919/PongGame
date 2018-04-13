ebeb1c4bf62b5c0c2873681e927e5474b5ad1c1e
shtoonkeyg <shtoonkeyg@gmail.com>
2018-04-12 22:07:08 -0600

    First iteration of working pong game

---
+++ pongHApr12/Displayables.java
@@ -0,0 +1,66 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.geom.Ellipse2D.Double;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+/** Class for slider objects */
+public class Displayables extends JComponent{
+    Slider rect1;
+    Slider rect2;
+    Ball ballOne;
+    Rectangle2D.Double usableRect1;
+    Rectangle2D.Double usableRect2;
+    Ellipse2D.Double usableBallOne;
+
+
+    /** creates object */
+    // Do we need to create modifiable coordinates too?
+    /** paints the object */
+    public Displayables() {
+        rect1 = new Slider(60,271);
+        rect2 = new Slider(1208,271);
+        ballOne = new Ball(631, 322);
+    }
+
+    public void movement(double y1Mod, double y2Mod, double ballX, double ballY) {
+        rect1.move(y1Mod);
+        rect2.move(y2Mod);
+        ballOne.move(ballX, ballY);
+    }
+
+    public void paintComponent(Graphics g) {
+        usableRect1 = rect1.getRect();
+        usableRect2 = rect2.getRect();
+        usableBallOne = ballOne.getBall();
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.BLACK);
+        g2.fill(usableRect1);
+        g2.fill(usableRect2);
+        g2.setColor(Color.BLUE);
+        g2.fill(usableBallOne);
+        }
+
+    public Dimension getPreferredSize() {
+        return new Dimension(1280,662); // appropriate constants
+    }
+
+    public double returnBallX() {
+        double xxx = ballOne.getBallX();
+        return xxx;
+    }
+
+    public double returnBallY() {
+        double yyy = ballOne.getBallY();
+        return yyy;
+    }
+
+    public double returnSlider1Y() {
+        double slide1Y = rect1.getSliderY();
+        return slide1Y;
+    }
+
+    public double returnSlider2Y() {
+        double slide2Y = rect2.getSliderY();
+        return slide2Y;
+    }
+}
